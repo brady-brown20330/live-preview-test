1a59568e033933dba97da238d4bd04ee44c7e05f
Brady <brown@Bradys-MacBook-Pro.local>
2022-05-02 11:15:38 -0400

    LP set up complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
+import Stack from './sdk/entry';
+import { onEntryChange } from "./sdk/entry.js";
 
 function App() {
+
+  const [header, setHeader] = useState([])
+
+  const getData = async () => {
+    const header = await Stack.getEntry("header", "en-us")
+    return header[0][0]
+  }
+
+  const updateData = () => {
+    const fetchedData = getData();
+    setHeader(fetchedData);
+  };
+
+  useEffect(() => {
+
+    getData().then((res) => {
+      setHeader(res)
+      console.log('I am the great and powerful res: ', res)
+    })
+
+    onEntryChange(updateData);
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          {header.title}
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {header.subtext}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/sdk/entry.js
@@ -0,0 +1,48 @@
+import * as contentstack from 'contentstack';
+
+import ContentstackLivePreview from '@contentstack/live-preview-utils';
+
+const Stack = contentstack.Stack({
+  api_key: process.env.REACT_APP_APIKEY,
+  delivery_token: process.env.REACT_APP_DELIVERY_TOKEN,
+  environment: process.env.REACT_APP_ENVIRONMENT,
+  region: process.env.REACT_APP_REGION ? process.env.REACT_APP_REGION : "us",
+  live_preview: {
+    management_token: process.env.REACT_APP_MANAGEMENT_TOKEN ,
+    enable: true,
+    host: 'api.contentstack.io'
+  },
+});
+
+Stack.setHost("api.contentstack.io")
+
+// initialize LP
+ContentstackLivePreview.init({
+  enable: true,
+  stackSdk: Stack,
+  ssr: false,
+});
+
+export const onEntryChange = ContentstackLivePreview.onEntryChange;
+
+
+export default {
+  getEntry(ctUid, locale) {
+    return new Promise((resolve, reject) => {
+      Stack.ContentType(ctUid)
+        .Query()
+        .language(locale)
+        .includeOwner()
+        .toJSON()
+        .find()
+        .then(
+          (result) => {
+            resolve(result)
+          },
+          (error) => {
+            reject(error)
+          }
+        )
+    })
+  }
+}
